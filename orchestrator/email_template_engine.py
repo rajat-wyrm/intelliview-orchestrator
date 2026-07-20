d59d342b77eaf5e739741edcc20aa429a12cce3a
Gone Umamahesh <YOUR_GITHUB_EMAIL>
2026-07-20 22:57:55 +0530

    from jinja2 import Template
    email_template = """
    <!DOCTYPE html>
    <html>
    
    <head>
        <title>Interview Invitation</title>
    </head>
    
    <body>
    
    <h2>Hello {{ name }},</h2>
    
    <p>
    You are invited for an interview.
    </p>
    
    <p>
    <b>Interview Time:</b> {{ interview_time }}
    </p>
    
    <p>
    <b>Interview Link:</b> {{ link }}
    </p>
    
    <p>
    Regards,<br>
    HR Team
    </p>
    
    </body>
    
    </html>
    """
    
    template = Template(email_template)
    
    recipients = [
        {
            "name": "Sai",
            "interview_time": "10:00 AM",
            "link": "https://meet.com/sai",
        },
        {
            "name": "Rahul",
            "interview_time": "12:30 PM",
            "link": "https://meet.com/rahul",
        },
        {
            "name": "Priya",
            "interview_time": "3:00 PM",
            "link": "https://meet.com/priya",
        },
    ]
    
    for person in recipients:
        result = template.render(
            name=person["name"],
            interview_time=person["interview_time"],
            link=person["link"],
        )
    
        print("==============================")
        print("Generated Email")
        print("==============================")
        print(result)

--- orchestrator/email_template_engine.py
+++ orchestrator/email_template_engine.py
@@ -1,4 +1,4 @@
-from jinja2 import Template
+ from jinja2 import Template
 
 # Email HTML Template
 email_template = """
@@ -35,27 +35,37 @@ HR Team
 </html>
 """
 
-
 # Create Jinja2 Template
 template = Template(email_template)
 
-
 # Recipient Data
 recipients = [
-    {"name": "Sai", "interview_time": "10:00 AM", "link": "https://meet.com/sai"},
-    {"name": "Rahul", "interview_time": "12:30 PM", "link": "https://meet.com/rahul"},
-    {"name": "Priya", "interview_time": "3:00 PM", "link": "https://meet.com/priya"},
+    {
+        "name": "Sai",
+        "interview_time": "10:00 AM",
+        "link": "https://meet.com/sai",
+    },
+    {
+        "name": "Rahul",
+        "interview_time": "12:30 PM",
+        "link": "https://meet.com/rahul",
+    },
+    {
+        "name": "Priya",
+        "interview_time": "3:00 PM",
+        "link": "https://meet.com/priya",
+    },
 ]
-
 
 # Generate Dynamic Emails
 for person in recipients:
     result = template.render(
-        name=person["name"], interview_time=person["interview_time"], link=person["link"]
+        name=person["name"],
+        interview_time=person["interview_time"],
+        link=person["link"],
     )
 
     print("==============================")
     print("Generated Email")
     print("==============================")
-
     print(result)
